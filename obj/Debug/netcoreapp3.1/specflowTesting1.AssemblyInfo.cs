1afb20377a7933ed3d2dcdc9d966ad81a4ee7ac5
Supreet Khattra <supreet.sekhon10@gmail.com>
2024-02-04 01:24:20 +1300

    removed john from list

--- obj/Debug/netcoreapp3.1/specflowTesting1.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/specflowTesting1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("specflowTesting1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77a62390ce3a001d77cf0ce74572e597f76dad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf365f3e4e7f6817aa7379d073e08fe0683666ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("specflowTesting1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("specflowTesting1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
